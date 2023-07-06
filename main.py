f4d56a304912aea88a79cc8cd1547ff04d992fba
HAWK1704 <omprakash_21462@aitpune.edu.in>
2023-07-07 01:17:04 +0530

    DAY #2

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 edgeBrowser = webdriver.Edge()
 edgeBrowser.get('https://google.com')
-edgeBrowser.find_element(By.NAME,"q").send_keys("github")
-options=edgeBrowser.find_elements(By.CSS_SELECTOR,'ul.G43f7e li span')
-print(len(options))
+edgeBrowser.find_element(By.NAME, "q").send_keys("github")
+
+# Wait for the options to be visible
+wait = WebDriverWait(edgeBrowser, 10)
+options = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/div/ul/li')))
+
 for i in options:
     print(i.text)
-sleep(1)
 edgeBrowser.quit()
